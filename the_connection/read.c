4aaab340598052cb47f70f7349075281217ed452
Petr Vaněk <pv@excello.cz>
2020-10-12 17:51:00 +0200

    botnet_client: solve

---
+++ the_connection/read.c
@@ -0,0 +1,14 @@
+#include <stdint.h>
+#include <stdio.h>
+#include <endian.h>
+
+int main(int argc, char *argv[]) {
+	int64_t len;
+	char buf[1024];
+	fread(&len, 1, sizeof len, stdin);
+	len = be64toh(len);
+	fprintf(stderr, "len: %d\n", len);
+	fread(buf, 1, len, stdin);
+	fwrite(buf, 1, len, stdout);
+	return 0;
+}
